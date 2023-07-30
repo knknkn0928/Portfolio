808e84fa1efba2bfe4c6c911ad11736d8da269a7
knknkn0928 <knknkn0928@naver.com>
2023-07-31 01:37:00 +0900

    wine 서브페이지 완료, 부분 수정

--- project_publishing/project5_wine/randing_page/randing_index.js
+++ project_publishing/project5_wine/randing_page/randing_index.js
@@ -1,5 +1,34 @@
 const section = document.querySelector('section');
 const open = document.querySelector('.open');
-open.addEventListener('click', e=>{
+const menu = document.querySelector('.left');
+
+open.addEventListener('click', e => {
     section.classList.add('on');
+    section.classList.add('active');
 })
+
+
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+
+var $html = $("html");
+var page = 1;
+var lastPage = $(".page").length;
+
+var timeScrollAnimation = 750;
+
+$html.animate({scrollTop:0},10);
+$(window).on("wheel", function(e){
+ 
+	if($html.is(":animated")) return;
+	if(e.originalEvent.deltaY > 0){
+		if(page== lastPage) return;
+		page++;
+	}else if(e.originalEvent.deltaY < 0){
+		if(page == 1) return;
+		page--;
+	}
+	var posTop = (page-1) * ($(window).height());
+	$html.animate({scrollTop : posTop}, timeScrollAnimation);
+});
