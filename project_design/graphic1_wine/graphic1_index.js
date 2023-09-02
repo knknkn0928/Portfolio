4e6a2c14f6002a6acc87a17e91727f6fbfe33e4e
knknkn0928 <knknkn0928@naver.com>
2023-09-03 01:39:46 +0900

    그래픽 파트 수정

--- project_design/graphic1_wine/graphic1_index.js
+++ project_design/graphic1_wine/graphic1_index.js
@@ -1,39 +1,18 @@
-
 $(function(){
-    $(".btn_black").click(function(){
-      $(".package_img").fadeOut(500, function(){
-        $(".package_img").attr("src", "./images/back_1.png");
-        $(".package_img").fadeIn(500);
-      });
-      $(".container2 img").fadeOut(510, function(){
-        $(".container2 img").attr("src", "./images/design2.png");
-        $(".container2 img").fadeIn(510);
-      });
+    let tabBtn = $(".tab_btn > ul > li");
+    let tabCont = $(".tab_cont > div");
+
+    tabCont.hide().eq(0).show();
+
+    tabBtn.click(function(){
+        const index = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
     });
-  });
-  
-  $(function(){
-    $(".btn_red").click(function(){
-      $(".package_img").fadeOut(500, function(){
-        $(".package_img").attr("src", "./images/back_2.png");
-        $(".package_img").fadeIn(500);
-      });
-      $(".container2 img").fadeOut(510, function(){
-        $(".container2 img").attr("src", "./images/design1.png");
-        $(".container2 img").fadeIn(510);
-      });
-    });
-  });
-  
-  $(function(){
-    $(".btn_gold").click(function(){
-      $(".package_img").fadeOut(500, function(){
-        $(".package_img").attr("src", "./images/back_3.png");
-        $(".package_img").fadeIn(500);
-      });
-      $(".container2 img").fadeOut(510, function(){
-        $(".container2 img").attr("src", "./images/design3.png");
-        $(".container2 img").fadeIn(510);
-      });
-    });
-  });
+});
+
+
+
+$("a").click(function(e){
+    e.preventDefault();
+});
